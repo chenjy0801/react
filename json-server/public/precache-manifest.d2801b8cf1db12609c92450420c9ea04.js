52bee252ac98c0df43091748bd58537ab2941d57
chenjy0801 <jc4805@columbia.edu>
2018-10-30 01:59:54 -0400

    json-server

---
+++ json-server/public/precache-manifest.d2801b8cf1db12609c92450420c9ea04.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "8b9f8640b99955ea1ed2",
+    "url": "/static/css/main.3c192652.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8db7c36a84d4f78ef92a",
+    "url": "/static/js/1.8db7c36a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b9f8640b99955ea1ed2",
+    "url": "/static/js/main.8b9f8640.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8db7c36a84d4f78ef92a",
+    "url": "/static/css/1.aed92467.chunk.css"
+  },
+  {
+    "revision": "a67e9548fa1df0ca8b1368cb5a3f5439",
+    "url": "/index.html"
+  }
+];
